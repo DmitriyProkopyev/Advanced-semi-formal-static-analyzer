068d9a0c6dbc7ed6c2db26a30e26ee3ef0d7e67d
almax07082005 <almaxim07082005@gmail.com>
2025-05-09 17:01:25 +0300

    add .idea folder to git

--- src/main/java/iu/sna/AdvancedSemiFormalStaticAnalyzerApplication.java
+++ src/main/java/iu/sna/AdvancedSemiFormalStaticAnalyzerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdvancedSemiFormalStaticAnalyzerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AdvancedSemiFormalStaticAnalyzerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedSemiFormalStaticAnalyzerApplication.class, args);
+    }
 }
