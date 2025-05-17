17b8b53b81eff7e6fa09cfdb0c6d18c7ef56572a
Aziz <aziz.yubuza@mail.ru>
2025-05-17 20:09:33 +0500

    integrate mardge

--- src/main/java/iu/sna/GraphCreator/LanguageAnalyzer/PydepsAnalyzer.java
+++ src/main/java/iu/sna/GraphCreator/LanguageAnalyzer/PydepsAnalyzer.java
@@ -6,6 +6,7 @@ import org.eclipse.jgit.diff.ContentSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -23,7 +24,8 @@ public class PydepsAnalyzer implements LanguageAnalyzer {
   private String PATH_TO_VENV;
   private static final Pattern DEPENDENCY_PATTERN = Pattern.compile("(\\w+)" +
           "\\s*->\\s*(\\w+)\\s*\\[.*?\\];");
-  public String runTool(List<String> fileParam) throws IOException {
+
+  private String runTool(List<String> fileParam) throws IOException {
     List<String> command = new ArrayList<>();
     command.add(PATH_TO_VENV + "/bin/pydeps");
     command.addAll(fileParam);
@@ -55,14 +57,15 @@ public class PydepsAnalyzer implements LanguageAnalyzer {
   }
 
   @Override
-  public List<Map.Entry<Path,Path>> analyzeDependencies(List<String> fileString) throws IOException {
+  public List<Map.Entry<Path, Path>> analyzeDependencies(
+          List<String> fileString) throws IOException {
     String toolOutput = runTool(fileString);
     Matcher matcher = DEPENDENCY_PATTERN.matcher(toolOutput);
     List<Map.Entry<Path, Path>> res = new ArrayList<>();
     while (matcher.find()) {
-     Path p1 = Paths.get(matcher.group(1));
-     Path p2 = Paths.get(matcher.group(2));
-     res.add(Map.entry(p1,p2));
+      Path p1 = Paths.get(matcher.group(1));
+      Path p2 = Paths.get(matcher.group(2));
+      res.add(Map.entry(p1, p2));
 
     }
     return res;
