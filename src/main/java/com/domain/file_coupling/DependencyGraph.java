b7d123e3677c1a2b56a88d7106fffc8cf9a5e210
Dmitriy Prokopyev <d.prokopyev@innopolis.university>
2025-05-18 21:14:40 +0300

    include-technology-stack-for-graph-construction

--- src/main/java/com/domain/file_coupling/DependencyGraph.java
+++ src/main/java/com/domain/file_coupling/DependencyGraph.java
@@ -1,12 +1,13 @@
 package com.domain.file_coupling;
 
+import com.domain.repository_scanner.FileTechnologyStack;
 import com.infrastructure.Graph;
 
 import java.io.File;
 import java.util.Collection;
 
-public class DependencyGraph extends Graph<ContextFile, Double> {
-    public DependencyGraph(Collection<File> files) {
+public class DependencyGraph extends Graph<FileTechnologyStack, Double> {
+    public DependencyGraph(Collection<FileTechnologyStack> files) {
         // build the entire graph
     }
 
